0d4e64a6be432f7334aabdef5e14f355d9bc1fe0
ekta sharma <ekta00sharma@gmail.com>
2021-04-06 11:26:17 -0700

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
+import React,{useState} from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
 import "fontsource-roboto";
 
 function App() {
-  return (
-    <div className="App">
-      <p>Hello Brother</p>
-    </div>
+
+   let  [userid, setUserid] = useState('');
+   const handleChange = (e)=>{
+     let data  = e.target.value;
+     //setUserid(data);
+   }
+   
+   return (
+    <form className ="App">
+    <label>
+     UserID :
+    <input type="text" name="name" onChange ={handleChange}/>
+   </label>
+   <br/>
+   <label>
+     Enter  Maximum Rating :
+    <input type="text" name="name" />
+  </label>
+  <br/>
+  <label>
+     Enter  Minimum Rating :
+    <input type="text" name="name" />
+  </label>
+  <br/>
+  <label>
+     Enter  Number of Problem :
+    <input type="text" name="name" />
+  </label>
+
+  <br/>
+   <label>
+     Enter your tags Name
+   </label>
+   <br/>
+  <div>
+   <span><Checkbox /> 
+     dp</span>
+     <span><Checkbox/> 
+     math</span>
+     <span><Checkbox/> 
+      implementation</span>
+  </div>  
+  <input type="submit" name="name" />   
+</form>
+    
   );
 }
 
